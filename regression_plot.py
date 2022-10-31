3dd9c9b5c6d92303878d8f9931e78d75ddb08a92
CaptainPickard <pickard.nicko@gmail.com>
2022-10-31 10:04:49 -0600

    Main function commit

--- regression_plot.py
+++ regression_plot.py
@@ -2,31 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import get_data
 
 
-data_frame = pd.read_csv('data\ADA_1day_data_1800day.csv')
+def run():
+    data_frame = pd.read_csv('data\ETH_1day_data_1800day.csv')
 
-time_frame = data_frame.open_time
-pd_cp = data_frame.close
+    time_frame = data_frame.open_time
+    pd_cp = data_frame.close
 
-x = time_frame.to_numpy()
-y = pd_cp.to_numpy()
+    x = time_frame.to_numpy()
+    y = pd_cp.to_numpy()
 
-x = x.reshape(-1,1)
-y = y.reshape(-1,1)
+    x = x.reshape(-1,1)
+    y = y.reshape(-1,1)
 
-regressor = LinearRegression()
-regressor.fit(x, y)
+    regressor = LinearRegression()
+    regressor.fit(x, y)
 
-y_pred = regressor.predict(x)
+    y_pred = regressor.predict(x)
 
-plt.scatter(x, y, color = 'blue')
-plt.plot(x, y_pred, color = 'black')
-plt.title('BTC Linear Regression Model')
-plt.xlabel('Time')
-plt.ylabel('Price Data in $Dollars')
-plt.xticks(())
-plt.yticks(())
-plt.show()
+    plt.scatter(x, y, color = 'black')
+    plt.plot(x, y_pred, color = 'orange')
+    plt.title('BTC Linear Regression Model')
+    plt.xlabel('Time')
+    plt.ylabel('Price Data in $Dollars')
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
 
 
+if __name__ == '__main__':
+    run()
